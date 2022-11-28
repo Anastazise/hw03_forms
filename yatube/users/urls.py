0255f676495a8c41b6cd69b08479a6482bd1bf2c
Anastazise <anastazidevilfor@yandex.ru>
2022-11-28 04:13:26 +0300

    finish

--- yatube/users/urls.py
+++ yatube/users/urls.py
@@ -6,14 +6,14 @@ app_name = 'users'
 
 urlpatterns = [
     path(
-      'logout/',
-      LogoutView.as_view(template_name='users/logged_out.html'),
-      name='logout'
+        'logout/',
+        LogoutView.as_view(template_name='users/logged_out.html'),
+        name='logout'
     ),
     path('signup/', views.SignUp.as_view(), name='signup'),
     path(
-      'login/',
-      LoginView.as_view(template_name='users/login.html'),
-      name='login'
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
     ),
 ]
