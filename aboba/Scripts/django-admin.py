586fb1837c605ae91689e97f1fa9397af3ec6e16
Anastazise <90971927+Anastazise@users.noreply.github.com>
2022-12-06 22:22:11 +0300

    delete venv

--- aboba/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\anast\desktop\яндекс практикум\hw03_forms\aboba\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
